c20179ef6bafa1b11acdf19d60d2a562e0dd981e
bluezsche <bluezsche@gmail.com>
2024-06-05 14:41:41 -0400

    test something

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import requests
 from dotenv import dotenv_values
 import os
-import pandas
 import sqlalchemy
 
 api_key = dotenv_values(".env")
 
-fred_url = f"https://api.stlouisfed.org/fred/category?category_id=125&api_key={api_key["key_fred"]}&file_type=json"
+fred_url = f"https://api.stlouisfed.org/fred/category?category_id=125&api_key={api_key['key_fred']}&file_type=json"
 
 header = {"Content-Type":"application/json", "Accept-Encoding":"deflate"}
 
